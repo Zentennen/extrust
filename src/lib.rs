c8405371e221bcedaddae448c7ff13e8561df99f
Zentennen <zentennen@gmail.com>
2021-10-28 22:57:31 +0200

    Implemented some io utilities

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,19 @@
-pub type Error = Box<dyn std::error::Error>;
-pub type Res<T> = Result<T, Error>;
+use std::error::Error;
+use std::io;
+use std::str::FromStr;
+use std::fmt::Display;
+
+pub type Er = Box<dyn Error>;
+pub type Res<T> = Result<T, Er>;
+
+pub fn cin<T>() -> Res<T> 
+where T: FromStr, <T as FromStr>::Err: Error + 'static
+{
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    Ok(input.trim().parse()?)
+}
+
+pub fn cout<T: Display>(output: &T){
+    println!("{}", output);
+}
