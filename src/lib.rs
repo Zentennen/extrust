d62e756f52b173b493b5c6f4d6448cd0854ac2d0
Olof Wikmark <zentennen@gmail.com>
2022-02-19 17:57:52 +0100

    Added anyhow and thiserror as dependencies and made cout not take a reference

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,17 @@ use std::io;
 use std::str::FromStr;
 use std::fmt::Display;
 
-pub type Er = Box<dyn Error>;
-pub type Res<T> = Result<T, Er>;
+pub type Er = anyhow::Error;
+pub type Res<T> = anyhow::Result<T>;
 
 pub fn cin<T>() -> Res<T> 
-where T: FromStr, <T as FromStr>::Err: Error + 'static
+where T: FromStr, <T as FromStr>::Err: Error + Send + Sync + 'static
 {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     Ok(input.trim().parse()?)
 }
 
-pub fn cout<T: Display>(output: &T){
+pub fn cout<T: Display>(output: T){
     println!("{}", output);
 }
